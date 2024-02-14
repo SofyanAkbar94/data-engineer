bea0d6ce426fed6478e3a50ca2d1e4d60ac534e6
SofyanAkbar94 <136363515+SofyanAkbar94@users.noreply.github.com>
2024-02-14 19:44:03 +0700

    Create append_2_generators.py

---
+++ 5_data_ingestion_with_dlt/append_2_generators.py
@@ -0,0 +1,15 @@
+def people_1():
+    for i in range(1, 6):
+        yield {"ID": i, "Name": f"Person_{i}", "Age": 25 + i, "City": "City_A"}
+
+# Example usage:
+for person in people_1():
+    print(person)
+
+def people_2():
+    for i in range(3, 9):
+        yield {"ID": i, "Name": f"Person_{i}", "Age": 30 + i, "City": "City_B", "Occupation": f"Job_{i}"}
+
+# Example usage:
+for person in people_2():
+    print(person)
